602b30eeb542a183ecac3182472eb96b1d57a0f3
seypak <138602201+seypak@users.noreply.github.com>
2023-08-10 11:15:03 +0900

    Time: 1 ms (77.95%), Space: 41.3 MB (90.15%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // 1. 캔디들 중 가장 큰 것을 추출한다.
         int max = 0;
         for(int i : candies) {
             if(i > max) {
@@ -8,6 +9,7 @@ class Solution {
         }
         
         List<Boolean> list = new ArrayList<Boolean>();
+        // 2. 캔디들 + 여분의캔디 >= 가장큰것 이면 true를 담아 리턴한다.
         for(int i : candies) {
             list.add(i+extraCandies >= max);
         }
